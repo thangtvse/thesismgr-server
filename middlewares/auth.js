1c42843eec56171f537a7cd0f3a344927b8e2063
Tran Viet Thang <uendno@gmail.com>
2016-11-11 10:09:20 +0700

    fix some bugs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,20 @@
  * Created by Tran Viet Thang on 10/22/2016.
  */
 
+var roles = require('../config/roles');
+
+exports.hasAccess = function (accessLevel) {
+    return function (req, res, next) {
+
+        if (req.isAuthenticated() && roles[accessLevel].indexOf(req.user.role) > -1) {
+            return next();
+        } else {
+            return res.redirect('/login');
+        }
+
+    }
+};
+
 exports.adminAuth = function (req, res, next) {
 
     // if user is authenticated in the session, carry on
