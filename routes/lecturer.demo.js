d4cae1c9f420f0ef1b506b429ff7717dd785bbfa
sunado <rockman.conan123@gmail.com>
2016-12-03 12:01:13 +0700

    update lec view

--- routes/lecturer.demo.js
+++ routes/lecturer.demo.js
@@ -14,7 +14,12 @@ router.get('/',[
 ]);
 router.get('/profile',[
     function (req,res) {
-        res.render('./users/lecturer/lecturer.update_info.ejs');
+        res.render('./users/lecturer/lecturer.profile.ejs');
+    }
+]);
+router.get('/edit-profile',[
+    function (req,res) {
+        res.render('./users/lecturer/lecturer.edit_profile.ejs');
     }
 ]);
 router.get('/pending',[
@@ -30,6 +35,17 @@ router.get('/accept',[
 router.get('/reject',[
     function (req,res) {
         res.render('./users/lecturer/lecturer.reject.ejs');
+    }
+]);
+router.get('/topic_details/:userId/:status/:id',[
+    function (req,res) {
+        res.render('./users/lecturer/lecturer.topic_details.ejs',{
+            name:"Nguyễn Văn A",
+            id:"14020644",
+            topic:"Thao tác sinh học phân tử cấp độ 1 ",
+            details:"   Tạo và lai giống các sinh vât nước ngọt đang có nguy cơ bị tuyệt chủng và nghiên cứu thông tin mã di truyên của ADN trong bộ nhiễm sắc thể tôm hùm do dó có thể đảo ngược được các vấn đề của hệ thông hiện tại đó là ai thì cũng chẳng quan trọng lắm.",
+            status: req.param("status")
+        });
     }
 ]);
 module.exports = router;
